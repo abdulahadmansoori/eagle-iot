7ea54fcea120f1d3d4ee716da0038d11a3217fa5
Abdul Ahad Mansoori <109919558+abdulahadmansoori@users.noreply.github.com>
2024-02-15 18:29:03 +0500

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,40 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
-
+import { Image, TouchableOpacity } from 'react-native';
+import { HeaderButtons, HeaderButton, Item } from 'react-navigation-header-buttons';
+import { Appbar, Button, IconButton, Text } from 'react-native-paper';
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}
+          // options={{
+          //   // headerStyle: {
+          //   //   backgroundColor: 'transparent', // Set background color to transparent
+          //   // },
+          //   headerTransparent: true, // Make header transparent
+          // }} 
+
+          options={{
+            headerLeft: () => (<Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="#252525"
+            />
+            ),
+            headerTitle: (props) => <Text>abc</Text>,
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                color="#252525"
+              />
+            ),
+          }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,21 +6,25 @@ import { View } from 'react-native';
 // import { View, Button, TextInput, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-paper';
 import { MyButton } from '../components/MyButton';
+import { MyHeader } from '../components/MyHeader';
 
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            {/* <Button mode="contained" onPress={() => navigation.navigate('Details', { itemId: 123 })}
+        <View>
+            {/* <MyHeader /> */}
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                {/* <Button mode="contained" onPress={() => navigation.navigate('Details', { itemId: 123 })}
                 style={{
                     backgroundColor: '#055FA8',
                     borderRadius: 5,
                 }}>
                 MyButton
             </Button> */}
-            <MyButton text={"MyButton"} func={() => navigation.navigate('Details', { itemId: 123 })} />
+                <MyButton text={"MyButton"} func={() => navigation.navigate('Details', { itemId: 123 })} />
 
-        </View>
+            </View>
+        </View> 
     );
 }
 
